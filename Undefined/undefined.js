9b3c3df29c33bc145f93248fe8cc6c44c1f31f4a
season shrestha <np03a170120@heraldcollege.edu.np>
2024-03-29 20:10:47 +0545

    feat: undefined

---
+++ Undefined/undefined.js
@@ -0,0 +1,13 @@
+console.log(x);
+var x = 10;
+console.log(x);
+
+var y = 90;
+if (y === undefined) {
+  console.log("undefined");
+} else {
+  console.log("defined");
+}
+
+var y = "ninety";
+console.log(y);
